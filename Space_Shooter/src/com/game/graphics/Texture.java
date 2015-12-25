579f69feb2e97c6199a3ded60d0053137ea694e0
rsfouro <b.sulicenko@gmail.com>
2015-12-25 18:23:53 +0100

    Added new classes, BufferedImageLoader, SpriteSheet and Texture

---
+++ Space_Shooter/src/com/game/graphics/Texture.java
@@ -0,0 +1,29 @@
+package com.game.graphics;
+
+import java.awt.image.BufferedImage;
+
+import com.game.objects.Player;
+
+public class Texture 
+{
+	SpriteSheet ss = null;
+
+	public BufferedImage[] player = new BufferedImage[1];
+	private BufferedImage spriteSheet = null;
+	
+	public Texture()
+	{
+		BufferedImageLoader loader = new BufferedImageLoader();
+		
+		spriteSheet = loader.loadImage("/SpriteSheet.png");
+		
+		ss = new SpriteSheet(spriteSheet);
+		
+		getTextures();
+	}
+	
+	private void getTextures()
+	{
+		player[0] = ss.grabImage(1, 1, 48, 48);
+	}
+}
